0119409686a869108b7e2093c07200d21dcdad8f
adnan9900 <108384414+adnan9900@users.noreply.github.com>
2022-06-29 11:38:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @Adnan
+› Base Ori @Adnan
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,26 +22,26 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6283817107067','6283817107067','6283817107067'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6283817107067'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6283817107067' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Adnan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Adnan-Bot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Adnan' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.ig = 'https://instagram.com/adnaannnn_'
+global.mygc = 'https://chat.whatsapp.com/HTeq6Fzh60RD5T3ZwIIiiY'
+global.myweb = 'https://s.id/ggggg'
+global.email = 'adnan8901@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.packname = '© Adnan' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
